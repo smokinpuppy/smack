a05c70a426992939dd9f86c4311d99526f4d7591
Robert Haddad <SmokinPuppy@Roberts-MacBook-Pro.local>
2017-11-26 13:58:50 +0200

    Created Login

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Robert Haddad on 11/26/17.
+//  Copyright © 2017 Robert Haddad. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var closeBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Robert Haddad on 11/26/17.
+//  Copyright © 2017 Robert Haddad. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
