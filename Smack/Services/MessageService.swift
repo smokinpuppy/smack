928e913c4833eeafd916828308d6ed9989bf2dea
Robert Haddad <SmokinPuppy@Roberts-MacBook-Pro.local>
2017-12-03 23:20:56 +0200

    Lesson 24

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -13,6 +13,7 @@ import SwiftyJSON
 class MessageService{
     static let instance = MessageService()
     
+    var messages = [Message]()
     var channels = [Channel]()
     var selectedChannel : Channel?
     
@@ -50,6 +51,44 @@ class MessageService{
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func findAllMessagesForChannel(channelId: String, completion: @escaping CompletionHandler){
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                self.clearMessages()
+                guard let data = response.data else { return }
+                do {
+                    if let json = try JSON(data: data).array {
+                        for item in json {
+                            let messageBody = item["messageBody"].stringValue
+                            let channelId = item["channelId"].stringValue
+                            let id = item["_id"].stringValue
+                            let userName = item["userName"].stringValue
+                            let userAvatar = item["userAvatar"].stringValue
+                            let userAvatarColor = item["userAvatarColor"].stringValue
+                            let timeStamp = item["timeStamp"].stringValue
+                            
+                            let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timestamp: timeStamp)
+                            self.messages.append(message)
+                        }
+                        print(self.messages)
+                        completion(true)
+                    }
+                } catch let error {
+                    print("Failed to Parse JSON #33 - Message Service: \(error)")
+                    completion(false)
+                }
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+    func clearMessages(){
+        messages.removeAll()
     }
     
     func clearChannels() {
